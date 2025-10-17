1b644718429056a9319989a71facf4a972bd01d6
Namibo06 <namibo654@gmail.com>
2025-10-16 21:03:09 -0300

    feat: criado método para verificar quando scroll se mexe para alteração o estilo visual do cabeçalho

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Header } from '../../shared/header/header';
 
@@ -18,6 +18,30 @@ export class Home{
   constructor(
     private readonly router: Router
   ){}
+
+  @HostListener('window:scroll', [])
+  verificarScroll(){
+    const headerContainer = document.getElementById('header-container');
+
+    const posicaoScroll = window.scrollY | document.documentElement.scrollTop;
+    if(posicaoScroll > 40){
+      if(headerContainer){
+        headerContainer.style.width = '95%';
+        headerContainer.style.left = '50%';
+        headerContainer.style.transform = 'translateX(-50%)';
+        headerContainer.style.borderBottomLeftRadius = '10px';
+        headerContainer.style.borderBottomRightRadius = '10px';
+      }
+    } else{
+      if(headerContainer){
+        headerContainer.style.width = '100%';
+        headerContainer.style.left = '0';
+        headerContainer.style.transform = 'translateX(0)';
+        headerContainer.style.borderBottomLeftRadius = '0px';
+        headerContainer.style.borderBottomRightRadius = '0px';
+      }
+    }
+  }
 
   redirecionarParaPesquisarTodos(tipoPesquisa: string){
     this.router.navigate(['/todos-itens'], {
